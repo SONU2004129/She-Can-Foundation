7853fa049ca37745242c1d43caf8c2e2a04b0962
Meghnath Marndi <meghmathmarndi@gmail.com>
2026-06-19 00:10:29 +0530

    this is finally the final with working maybe

--- src/components/FormSection.tsx
+++ src/components/FormSection.tsx
@@ -79,7 +79,7 @@ export default function FormSection({
             const API_URL =
                 import.meta.env.MODE === "development"
                     ? "http://localhost:3000"
-                    : "https://she-can-foundation-5uvu.onrender.com";
+                    : "https://she-can-foundation-l03c.onrender.com";
 
             const response = await fetch(`${API_URL}/api/submissions`, {
                 method: "POST",
